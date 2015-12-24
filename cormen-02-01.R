db4a0cd25ccc1d3c4bb70b9afc2a3ea15f6b26a8
serhatcevikel <serhatcevikel@yahoo.com>
2015-12-25 01:33:25 +0200

    solved 'replacement has zero length' problem

--- cormen-02-01.R
+++ cormen-02-01.R
@@ -2,13 +2,12 @@ sersortinsert01 <- function (x) { #x is a vector of numbers to be sorted
     for ( j in 2:length(x) ) { # loop for comparing element j
         key <- x[j] # comparing element 
         i <- j - 1 # index for element to be compared
-        while ( !is.null( (i > 0) & (x[i] > key) ) ) { # continue condition to compare
+        while ( (i > 0)  && (x[i] > key) ) { # continue condition to compare
             x[i + 1] <- x[i]; # shift the larger value right
-            #i <- i - 1; # shift compare index left
             x[i] <- key; # shift the comparing element -key- left  
-            #x[i + 1] <- key; # shift the comparing element -key- left 
             i <- i - 1; # shift compare index left
         }
     }
+return(x)
 }
  
