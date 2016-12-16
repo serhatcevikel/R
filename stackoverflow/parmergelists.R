e814fd3bb41b798650ed7327354ea6579456c6e6
Serhat Cevikel <serhatcevikel@yahoo.com>
2016-12-17 01:39:07 +0300

    new file

--- stackoverflow/parmergelists.R
+++ stackoverflow/parmergelists.R
@@ -6,11 +6,14 @@ A <- list(c(1,2,3), c(3,4,5), c(6,7,8))
 
 B <- list(c("a", "b", "c"), c("d", "e", "f"), c("g", "h", "i"))
 
-mergepar <- function(x = A, y = B) {
-    ln <- max(length(A), length(B))
-    newlist <- as.list(rep(NA, ln))
-    for (i in 1:ln) {
-        newlist[[i]] <- lapply(list(A, B), function(x) "[["("["(x, i), 1))
+mergepar <- function(x = A, y = B) { # merge two lists in parallel
+    ln <- max(length(x), length(y)) # max length
+    newlist <- as.list(rep(NA, ln)) # empty list of max length
+
+    for (i in 1:ln) { # for1, across length
+        # two level subsetting (first with [ and then [[, so no subscript out of bound error) and lapply
+        newlist[[i]] <- lapply(list(A, B), function(x) "[["("["(x, i), 1)) 
     }
+
     return(newlist)
 }
