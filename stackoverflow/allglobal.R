bf060205a6f8099bd65ed2f9eab381e25d61e7c6
Serhat Cevikel <serhatcevikel@yahoo.com>
2016-12-17 03:01:34 +0300

    new file

--- stackoverflow/allglobal.R
+++ stackoverflow/allglobal.R
@@ -1,11 +1,15 @@
 allglobal <- function() {
     lss <- ls(envir = parent.frame())
     for (i in lss) {
-        assign(lss[1], get(lss[1], envir = parent.frame()), envir = .GlobalEnv)
+        assign(i, get(i, envir = parent.frame()), envir = .GlobalEnv)
     }
 }
 
 testallglob <- function() {
     test1 <- 1
+    test2 <- function() 2
     allglobal()
 }
+
+# eval(parse(text=sprintf("class(%s)", lss[1])))
+
