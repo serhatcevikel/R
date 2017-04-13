af9a5391996c710542290d947b8a4d911077c96d
Serhat Cevikel <serhatcevikel@yahoo.com>
2017-04-13 18:14:43 +0300

    modified

---
+++ other/gpuR.R
@@ -0,0 +1,13 @@
+# gpuR benchmark
+
+library(gpuR)
+
+ORDER <- 2048
+
+A = matrix(rnorm(ORDER^2), nrow=ORDER)
+B = matrix(rnorm(ORDER^2), nrow=ORDER)
+gpuA = gpuMatrix(A, type="double")
+gpuB = gpuMatrix(B, type="double")
+
+matmult1 <- function() A %*% B
+matmult2 <- function() gpuA %*% gpuB
