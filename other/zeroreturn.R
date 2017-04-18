bde337dea25c793662e60980260fb1d28e18f315
Serhat Cevikel <serhatcevikel@yahoo.com>
2017-04-18 17:15:04 +0300

    modified

--- other/zeroreturn.R
+++ other/zeroreturn.R
@@ -1,8 +1,14 @@
 # get share of zero return counts in each month
 
-countifzero <- function() {
-    data1 <- read.csv("Countif.csv")
-    averages <- aggregate(data1[,-(1:2)], by = list(data1[,1], data1[,2]), FUN = function(x) length(x[x == 0]) / length(x))
-    write.csv(averages, file = "averages.csv")
-    return(averages)
+countifzero <- function() { # count the zero returns in each month for each column
+
+    filelist <- list.files("./input") # get file list under current directory
+
+    for (filn in filelist) { # for1, across csv files
+        data1 <- read.csv(filen) # read data
+        output <- gsub(".csv", "-zero.csv", filn) # create output object name 
+        averages <- aggregate(data1[,-(1:2)], by = list(data1[,1], data1[,2]), FUN = function(x) length(x[x == 0]) / length(x)) # get averages
+        write.csv(averages, file = sprintf("./output/%s", output)) # write to file
+    } # close for1
+
 }
