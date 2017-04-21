01ad1f4d3f612d4e27e992e92dd0bc2e040dc26c
Serhat Cevikel <serhatcevikel@yahoo.com>
2017-04-21 14:31:15 +0300

    modified

--- other/carfzero/zeroreturn.R
+++ other/carfzero/zeroreturn.R
@@ -1,13 +1,14 @@
 # get share of zero return counts in each month
 
-countifzero <- function(path = ".") { # count the zero returns in each month for each column
+countifzero <- function(path = ".", sepp = ";") { # count the zero returns in each month for each column
     
     setwd(path) # change working directory
     dir.create(file.path(".", "output"), showWarnings = F) # create output directory
     filelist <- list.files("./input") # get file list under current directory
 
     for (filn in filelist) { # for1, across csv files
-        data1 <- read.csv(filen) # read data
+        #data1 <- read.csv(sprintf("./input/%s", filn)) # read data
+        data1 <- read.table(sprintf("./input/%s", filn), header = T, sep = sepp) # read data
         output <- gsub(".csv", "-zero.csv", filn) # create output object name 
 
         averages <- aggregate(data1[,-(1:2)],
