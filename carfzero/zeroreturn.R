39a2c7a097f04940b14cf3e6df3122a6e68ace73
Serhat Cevikel <serhatcevikel@yahoo.com>
2017-08-02 15:16:48 +0300

    modified

--- carfzero/zeroreturn.R
+++ carfzero/zeroreturn.R
@@ -1,3 +1,5 @@
+options(scipen = 999)
+
 # get share of zero return counts in each month
 #library("xlsx") #include library xlsx to read data from xls file
 library("openxlsx") #include library xlsx to read data from xls file
@@ -12,12 +14,19 @@ countifzero <- function(path = ".", sepp = ";") { # count the zero returns in ea
     for (filn in filelist) { # for1, across csv files
         #data1 <- read.csv(sprintf("./input/%s", filn)) # read data
         data1 <- read.table(sprintf("./input/%s", filn), header = T, sep = sepp) # read data
+        #data1 <- openxlsx::read.xlsx(sprintf("./input.xls/%s", filn)) # read sudoku data from file into a matrix names sudoku01
         output <- gsub(".csv", "-zero.csv", filn) # create output object name 
+        NTfn <- gsub(".csv", "-NT.csv", filn) # create NT object name
+
+        NT <- apply(data1, 1, function(x) length(x[x == 0 & !is.na(x)]) / length(x[!is.na(x)]) > 0.9)
+
+        data1 <- data1[!NT,]
 
         averages <- aggregate(data1[,-(1:2)],
                               by = list(data1[,1], data1[,2]),
                               FUN = function(x) length(x[x == 0 & !is.na(x)]) / length(x[!is.na(x)])) # get averages
 
+        write.csv(NT, file = sprintf("./NT/%s", NTfn)) # write NT to file
         write.csv(averages, file = sprintf("./output/%s", output)) # write to file
     } # close for1
 
@@ -66,6 +75,7 @@ covbymonth.xls <- function(path = ".", sepp = ";", lagg = 1) { # count the zero 
     } # close for1
 
 }
+
 covna <- function(x, lagg = 1) { # lagged covariances
     x <- x[!is.na(x)] # remove NAs
 
